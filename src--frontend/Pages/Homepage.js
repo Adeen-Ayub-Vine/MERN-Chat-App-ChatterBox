65c1e8b3d5d06d410f16c3a0662b7623ac02a9f8
Adeen Ayub Vine <adeen.ayub.vine@gmail.com>
2024-06-23 23:05:03 +0500

    Added Realtime Notifications

---
+++ src--frontend/Pages/Homepage.js
@@ -0,0 +1,78 @@
+import React, { useEffect } from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import SignUp from "../components/Authentication/SignUp";
+import { useNavigate } from "react-router-dom";
+
+const Homepage = () => {
+  //if user is logged in, send to chat page
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (userInfo) {
+      navigate("/chats");
+    }
+  }, [navigate]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          fontFamily="Work sans"
+          color="black"
+          display="flex"
+          justifyContent="center"
+        >
+          Chatter-Box
+        </Text>
+      </Box>
+
+      <Box
+        p={4}
+        bg={"white"}
+        w="100%"
+        borderRadius="lg"
+        borderWidth="1px"
+        color="black"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">SignUp</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
